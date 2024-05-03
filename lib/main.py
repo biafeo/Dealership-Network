29653d80e5c8e3b8a4f4cac2d4d0147bebf019cc
aaronalex15 <aaronalex1230@gmail.com>
2024-05-03 10:11:32 -0700

    defined the classes, added mock data for testing

---
+++ lib/main.py
@@ -0,0 +1,14 @@
+# import argparse
+
+# def main():
+#     parser = argparse.ArgumentParser(description='A basic CLI application')
+#     parser.add_argument('-g', '--greet', help='Greet the user')
+#     args = parser.parse_args()
+
+#     if args.greet:
+#         print('Hello, {}!'.format(args.greet))
+#     else:
+#         print('Hello!')
+
+# if __name__ == "__main__":
+#     main()
